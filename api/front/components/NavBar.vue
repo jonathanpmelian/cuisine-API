e5c8fa76a0ac95d0e99644f6349dce91dadc2b8f
Jonathan Pulido <jonathanpmelian@gmail.com>
2022-05-18 23:26:58 +0100

    GENERAL: NavBar, Carousel Fix

--- api/front/components/NavBar.vue
+++ api/front/components/NavBar.vue
@@ -1,41 +1,24 @@
 <template>
-    <v-app-bar    
-      fixed
-      elevate-on-scroll
-      scroll-target="#scrolling-techniques-7"
-      max-height="70px"
-      :color="windowTop"
-    >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Title</v-toolbar-title>
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-app-bar>
+  <v-app-bar fixed elevate-on-scroll :color="navBarColor">
+    <v-row>
+      <v-col>Title</v-col>
+    </v-row>
+  </v-app-bar>
 </template>
 
 <script>
-    export default {
-        name: 'NavBar',
-        data(){ 
-          return { 
-            windowTop: false 
-            };
-             }, 
-        mounted() { 
-          window.addEventListener("scroll", () => { this.windowTop = window.scrollY > 0 ? 'blue' : 'transparent'; 
-          }
-          ); 
-          } 
+export default {
+  name: 'NavBar',
+  data() {
+    return {
+      navBarColor: 'transparent',
     }
+  },
+  mounted() {
+    window.onscroll = () =>
+      (this.navBarColor = window.scrollY > 0 ? 'blue' : 'transparent')
+  },
+}
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
